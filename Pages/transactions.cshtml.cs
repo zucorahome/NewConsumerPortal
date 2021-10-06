fee583acc1d1804642ffd046e20a7485b05d482c
Himadri Vyas <himadri.vyas@zucora.com>
2021-10-06 13:21:48 -0400

    Till Account details pages completed without navigation

---
+++ Pages/transactions.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace New_ConsumerPortal.Pages
+{
+    public class transactionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
